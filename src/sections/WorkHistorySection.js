0d87aed3ee966808a953e1d7ca06ceb4ce1e1628
jesseraypurdy <jesseraypurdy@gmail.com>
2023-11-12 10:15:17 +1300

    Reversed work order

--- src/sections/WorkHistorySection.js
+++ src/sections/WorkHistorySection.js
@@ -24,16 +24,15 @@ class WorkHistorySection extends PureComponent {
 				<Fade bottom>
 					<Box pb={padding}>
 						<HorizontalCard
-							title="Paymark - Auckland, NZ"
-							subtitle=".Net Software Engineer | Jan 2020 - Nov 2021"
-							img={paymarkLogo}
+							title="Worldline NZ - Auckland, NZ"
+							subtitle="Lead .Net Engineer | July 2022 - present"
+							img={worldlineLogo}
 						>
-							Paymark is New Zealand's leading payments innovation company. My responsibilities as a
-							software engineer included, but were not limited to: Front and backend feature development
-							and maintenance of the monolithic Click web payments product. Proxy and Java microservice
-							maintenance. Automated test suite development, including both UI automation and performance
-							testing. Rotational on-call duties, and production deployments. I was in a fairly large
-							Scrum team and eventually led a platform improvement initiative.
+							I returned to Worldline NZ (formerly Paymark) to begin my first formal Leadership role. In
+							this position I balance people, and technical leadership while working as a hands on
+							engineer. My day-to-day includes, but is not limited to one-on-ones, mentoring, maintaining
+							and building new features for the Click platform, offering high-level technical guidance,
+							planning, and discussing process improvements.
 						</HorizontalCard>
 					</Box>
 				</Fade>
@@ -55,15 +54,16 @@ class WorkHistorySection extends PureComponent {
 				<Fade bottom>
 					<Box pb={padding}>
 						<HorizontalCard
-							title="Worldline NZ - Auckland, NZ"
-							subtitle="Lead .Net Engineer | July 2022 - present"
-							img={worldlineLogo}
+							title="Paymark - Auckland, NZ"
+							subtitle=".Net Software Engineer | Jan 2020 - Nov 2021"
+							img={paymarkLogo}
 						>
-							I returned to Worldline NZ (formerly Paymark) to begin my first formal Leadership role. In
-							this position I balance people, and technical leadership while working as a hands on
-							engineer. My day-to-day includes, but is not limited to one-on-ones, mentoring, maintaining
-							and building new features for the Click platform, offering high-level technical guidance,
-							planning, and discussing process improvements.
+							Paymark is New Zealand's leading payments innovation company. My responsibilities as a
+							software engineer included, but were not limited to: Front and backend feature development
+							and maintenance of the monolithic Click web payments product. Proxy and Java microservice
+							maintenance. Automated test suite development, including both UI automation and performance
+							testing. Rotational on-call duties, and production deployments. I was in a fairly large
+							Scrum team and eventually led a platform improvement initiative.
 						</HorizontalCard>
 					</Box>
 				</Fade>
